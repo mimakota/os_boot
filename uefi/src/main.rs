1de380863e37bde4e74ea9635e76cc097d05dc08
mimakota <mimakota624@gmail.com>
2025-04-19 00:06:52 +0900

    Exited boot services!

--- uefi/src/main.rs
+++ uefi/src/main.rs
@@ -1,13 +1,27 @@
 #![no_main]
 #![no_std]
+#[macro_use]
 
-use log::info;
+extern crate alloc;
 use uefi::prelude::*;
 
 #[entry]
-fn main(_image_handle: Handle, mut system_table: SystemTable<Boot>)  -> Status{ 
+fn main(handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     uefi_services::init(&mut system_table).unwrap();
-    info!("Hello world");
-    //loop {}
-    return Status::SUCCESS
+
+    log::info!("Start of UEFI app");
+
+    let mut mmap_buf = vec![0u8; 16384];
+
+    let _mem_map = system_table
+        .boot_services()
+        .memory_map(&mut mmap_buf)
+        .expect("Failed to get memory map");
+
+    log::info!("Got memory map");
+    
+    log::info!("Exited boot services!");
+
+    let (_system_table, _final_map) = system_table.exit_boot_services();
+    Status::SUCCESS
 }
